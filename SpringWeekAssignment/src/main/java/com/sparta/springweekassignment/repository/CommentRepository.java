e5a2c74bac133bd36efc0c78b0d5e05984697069
Kangjunesu <kulbit92@naver.com>
2023-04-27 17:03:34 +0900

    Lv3시작 // Class파일 생성

---
+++ SpringWeekAssignment/src/main/java/com/sparta/springweekassignment/repository/CommentRepository.java
@@ -0,0 +1,4 @@
+package com.sparta.springweekassignment.repository;
+
+public class CommentRepository {
+}
---
+++ SpringWeekAssignment/src/main/java/com/sparta/springweekassignment/service/CommentService.java
@@ -0,0 +1,4 @@
+package com.sparta.springweekassignment.service;
+
+public class CommentService {
+}
